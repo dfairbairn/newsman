64c7b1d30424d0fa431a94e3eb41399eb2fa2b5d
pm.me.ur.0dayz <pm.me.ur.0days@gmail.com>
2026-04-18 21:34:04 -0400

    Add print statements to token creation util

--- util_create_token.py
+++ util_create_token.py
@@ -1,3 +1,4 @@
+print(f"0. Loading")
 # create_token.py
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
@@ -5,12 +6,15 @@ import json
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']  # adjust scope as needed
 
+print(f"1.")
 # 1) Make sure you have client_secret.json downloaded from Google Cloud Console
 flow = InstalledAppFlow.from_client_secrets_file('client3_secret.json', SCOPES)
 
+print(f"2.")
 # This opens a browser and completes the OAuth flow interactively.
 creds = flow.run_local_server(port=0)
 
+print(f"3.")
 # Save the authorized user credentials (this file is what from_authorized_user_file expects)
 with open('token.json', 'w') as f:
     f.write(creds.to_json())
